c5dd7c86b7cd6fab536e9bd9310ee453fcdbc92f
OVIEGREAT <sxsxazyzyyy54@gmail.com>
2024-06-07 22:56:21 +0100

    Add files via upload

---
+++ Exe 1.c
@@ -0,0 +1,33 @@
+// OVIE GREAT IMONIEFE
+// D CODE: D10110299
+// MATRIC NUMBER:CMP2307639
+// Print positive integers from 1 to 10 using for loop
+#include <stdio.h>
+
+int main() {
+    printf("Printing positive integers from 1 to 10 using for loop:\n");
+    for (int i = 1; i <= 10; i++) {
+        printf("%d ", i);
+    }
+    printf("\n\n");
+
+    // Print positive integers from 1 to 10 using while loop
+    printf("Printing positive integers from 1 to 10 using while loop:\n");
+    int j = 1;
+    while (j <= 10) {
+        printf("%d ", j);
+        j++;
+    }
+    printf("\n\n");
+
+    // Print positive integers from 1 to 10 using do-while loop
+    printf("Printing positive integers from 1 to 10 using do-while loop:\n");
+    int k = 1;
+    do {
+        printf("%d ", k);
+        k++;
+    } while (k <= 10);
+    printf("\n");
+
+    return 0;
+}
